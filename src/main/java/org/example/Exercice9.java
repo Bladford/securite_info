e447a3ae5e35827bb0f17c11d4529ed7815ace17
Bladford <ocean.maurouard@gmail.com>
2023-11-19 01:49:31 +0100

    corrction mineur

--- src/main/java/org/example/Exercice9.java
+++ src/main/java/org/example/Exercice9.java
@@ -1,46 +1,72 @@
 package org.example;
-
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Exercice9 {
+
+    private static BigInteger moduleAlice;
+    private static BigInteger moduleBob;
+    private static final int NUM_THREADS = 200; // Nombre de threads à utiliser
+
     public static void main(String[] args) {
         try {
-            String pathAlice = "src/main/resources/pk_alice";
-            String pathBob = "src/main/resources/pk_alice";
+            // Lire les modules depuis les fichiers
+            System.out.println("Lecture des modules RSA...");
+            moduleAlice = new BigInteger(Files.readAllBytes(Paths.get("src/main/resources/pk_alice")));
+            moduleBob = new BigInteger(Files.readAllBytes(Paths.get("src/main/resources/pk_bob")));
 
-            String moduleAlice = new String(Files.readAllBytes(Paths.get(pathAlice))).trim();
-            String moduleBob = new String(Files.readAllBytes(Paths.get(pathBob))).trim();
+            System.out.println("Module Alice: " + moduleAlice);
+            System.out.println("Module Bob: " + moduleBob);
 
-            BigInteger nAlice = new BigInteger(moduleAlice);
-            BigInteger nBob = new BigInteger(moduleBob);
+            // Utiliser le multithreading pour accélérer le processus
+            ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
 
-            System.out.println("Factorisation des modules...");
-            BigInteger[] factorsAlice = factorize(nAlice);
-            BigInteger[] factorsBob = factorize(nBob);
+            BigInteger maxLimit = moduleAlice.sqrt().add(BigInteger.ONE);
+            BigInteger range = maxLimit.divide(BigInteger.valueOf(NUM_THREADS));
 
-            System.out.println("Alice: p = " + factorsAlice[0] + ", q = " + factorsAlice[1]);
-            System.out.println("Bob: p = " + factorsBob[0] + ", q = " + factorsBob[1]);
-        } catch (Exception e) {
+            System.out.println("Début de la factorisation en utilisant " + NUM_THREADS + " threads...");
+
+            for (int i = 0; i < NUM_THREADS; i++) {
+                BigInteger start = range.multiply(BigInteger.valueOf(i)).add(BigInteger.valueOf(2));
+                BigInteger end = start.add(range);
+
+                executor.submit(new FactorFinder(start, end, moduleAlice, moduleBob));
+            }
+
+            executor.shutdown();
+            if (!executor.awaitTermination(1, TimeUnit.HOURS)) {
+                System.out.println("La factorisation n'a pas abouti dans le temps imparti.");
+            }
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static BigInteger[] factorize(BigInteger n) {
-        BigInteger two = new BigInteger("2");
+    static class FactorFinder implements Runnable {
+        private final BigInteger start;
+        private final BigInteger end;
+        private final BigInteger moduleAlice;
+        private final BigInteger moduleBob;
 
-        if (n.mod(two).equals(BigInteger.ZERO)) {
-            return new BigInteger[]{two, n.divide(two)};
+        public FactorFinder(BigInteger start, BigInteger end, BigInteger moduleAlice, BigInteger moduleBob) {
+            this.start = start;
+            this.end = end;
+            this.moduleAlice = moduleAlice;
+            this.moduleBob = moduleBob;
         }
 
-        BigInteger i = new BigInteger("3");
-        while (i.multiply(i).compareTo(n) <= 0) {
-            if (n.mod(i).equals(BigInteger.ZERO)) {
-                return new BigInteger[]{i, n.divide(i)};
+        @Override
+        public void run() {
+            for (BigInteger num = start; num.compareTo(end) < 0; num = num.add(BigInteger.ONE)) {
+                if (moduleAlice.mod(num).equals(BigInteger.ZERO) && moduleBob.mod(num).equals(BigInteger.ZERO)) {
+                    System.out.println("Facteur commun trouvé : " + num);
+                }
             }
-            i = i.add(two);
         }
-        return new BigInteger[]{n, BigInteger.ONE};
     }
 }
