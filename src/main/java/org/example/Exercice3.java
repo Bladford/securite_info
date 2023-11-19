bb090aa8419678900415423bf626704ca30b1c04
Bladford <ocean.maurouard@gmail.com>
2023-11-19 13:22:32 +0100

    passage de l'exo  en multithreading

--- src/main/java/org/example/Exercice3.java
+++ src/main/java/org/example/Exercice3.java
@@ -11,26 +11,28 @@ import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
-
-import static java.lang.Thread.sleep;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Exercice3 {
     static String hash = "5a74dd4eef347734c8a0a9a3188abd11";
+    static AtomicBoolean found = new AtomicBoolean(false);
 
     public static void main(String[] args) {
         // Dézippe le fichier src/main/resources/rockyou.zip
         try {
             ZipFile zipFile = new ZipFile("src/main/resources/rockyou.zip");
             zipFile.extractAll("src/main/resources/");
-
         } catch (ZipException e) {
             e.printStackTrace();
             System.out.println("Erreur lors de la décompression du fichier rockyou.zip");
-            return; // Arrête l'exécution si une erreur se produit lors de la décompression
+            return;
         }
 
+        int numberOfThreads = 8; // Nombre de threads à utiliser
         try {
-            findPassword();
+            findPassword(numberOfThreads);
         } catch (NoSuchAlgorithmException e) {
             System.err.println("Algorithme de hachage non supporté : " + e.getMessage());
         } catch (IOException e) {
@@ -44,33 +46,59 @@ public class Exercice3 {
         }
     }
 
-
-    public static void findPassword() throws NoSuchAlgorithmException, IOException {
-        // Charger le fichier de mots de passe
+    public static void findPassword(int numberOfThreads) throws NoSuchAlgorithmException, IOException {
         List<String> passwords = Files.readAllLines(Paths.get("src/main/resources/rockyou.txt"), StandardCharsets.UTF_8);
 
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            int finalI = i;
+            executor.submit(() -> {
+                try {
+                    processPasswords(passwords, numberOfThreads, finalI, startTime);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            // Attendre que tous les threads terminent
+        }
+
+        if (!found.get()) {
+            long endTime = System.currentTimeMillis();
+            System.out.println("Mot de passe non trouvé");
+            System.out.println("Temps d'exécution : " + (endTime - startTime) + " ms");
+        }
+    }
+
+    private static void processPasswords(List<String> passwords, int numberOfThreads, int threadIndex, long startTime) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
+        int passwordCount = 0;
 
-        long startTime = System.currentTimeMillis(); // Enregistre le temps de début
-        int passwordCount = 0; // Compteur pour le nombre de mots de passe testés
+        for (int i = threadIndex; i < passwords.size(); i += numberOfThreads) {
+            if (found.get()) {
+                return; // Si un autre thread a trouvé le mot de passe
+            }
 
-        for (String password : passwords) {
+            String password = passwords.get(i);
             md.reset();
             byte[] digest = md.digest(password.getBytes(StandardCharsets.UTF_8));
             String myHash = bytesToHex(digest);
-            passwordCount++; // Incrémente le compteur à chaque mot de passe testé
+            passwordCount++;
+
             if (myHash.equalsIgnoreCase(hash)) {
-                long endTime = System.currentTimeMillis(); // Enregistre le temps de fin
+                found.set(true);
+                long endTime = System.currentTimeMillis();
                 System.out.println("Le mot de passe est : " + password);
                 System.out.println("Nombre de mots de passe testés : " + passwordCount);
                 System.out.println("Temps d'exécution : " + (endTime - startTime) + " ms");
                 return;
             }
         }
-        long endTime = System.currentTimeMillis(); // Enregistre le temps de fin si le mot de passe n'est pas trouvé
-        System.out.println("Mot de passe non trouvé");
-        System.out.println("Nombre de mots de passe testés : " + passwordCount);
-        System.out.println("Temps d'exécution : " + (endTime - startTime) + " ms");
     }
 
     private static String bytesToHex(byte[] bytes) {
